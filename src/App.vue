e0d64c12a3aaec4a3a01a7b4a7671d40ab331da2
njayman <najishm828282@gmail.com>
2021-04-17 05:47:14 +0600

    started a new vue project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloNjay msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import HelloNjay from "./components/HelloNjay.vue";
+export default {
+  name: "App",
+  components: {
+    HelloNjay,
+  },
+};
+</script>
---
+++ src/components/HelloNjay.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello visitors?</h1>
+  <h2>My name in <b>NJAYMAN</b></h2>
+  <p>Its an honor to meet you</p>
+  <p>
+    Follow me on
+    <a href="https://github.com/njayman" target="__blank">my github account</a>
+  </p>
+</template>
+<script>
+export default {
+  name: "HelloNjay",
+};
+</script>
